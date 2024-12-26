0eb3f5177d9dc81f9a3e6187abf88e95d7fb9d5d
Elena Dobruk <elena.dobruk@yandex.ru>
2024-12-26 16:55:09 +0300

    add settings configuration from .env

--- app/core/config.py
+++ app/core/config.py
@@ -1,1 +1,16 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from functools import lru_cache
+
+
+class Settings(BaseSettings):
+    API_KEY: str
+    SECRET_KEY: str
+    ALGORITHM: str
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+
+    model_config = SettingsConfigDict(env_file='.env')
+
+
+@lru_cache
+def get_settings():
+    return Settings()
