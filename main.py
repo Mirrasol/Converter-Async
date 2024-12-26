a532ad760f2f4226115600b784f0860d261424e1
Elena Dobruk <elena.dobruk@yandex.ru>
2024-12-26 12:52:47 +0300

    fix an error in router names

--- main.py
+++ main.py
@@ -3,13 +3,10 @@ from fastapi import FastAPI
 from app.api.endpoints.users import auth_router
 from app.api.endpoints.currency import currency_router
 
-
 app = FastAPI()
 
-app.include_router(
-    auth_router,
-    currency_router,
-)
+app.include_router(auth_router)
+app.include_router(currency_router)
 
 if __name__ == "__main__":
     uvicorn.run(app="main:app")
