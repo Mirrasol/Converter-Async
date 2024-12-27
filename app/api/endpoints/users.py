175fb3861650d4f7cdc63bb04759ea7b23680446
Elena Dobruk <elena.dobruk@yandex.ru>
2024-12-27 16:52:57 +0300

    add user registration and login routes

--- app/api/endpoints/users.py
+++ app/api/endpoints/users.py
@@ -1,4 +1,9 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordRequestForm
+from typing import Annotated
+from app.api.schemas.users import User
+from app.db.db_manager import add_user, get_user
+from app.core.security import create_token
 
 auth_router = APIRouter(
     prefix='/auth',
@@ -6,11 +11,27 @@ auth_router = APIRouter(
 )
 
 
-@auth_router.post('/auth/login')
-def login():
-    pass
-
-
 @auth_router.post('/auth/register')
-def register_user():
-    pass
+def register_user(user: User):
+    username_to_check = user.username
+    if get_user(username_to_check):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail='User already exists',
+            headers={'WWW-Authenticate': 'Bearer'},
+        )
+    else:
+        add_user(user)
+        return {'message': 'You have successfully registered!'}
+
+
+@auth_router.post('/auth/login')
+def login(user_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
+    user_data_from_db = get_user(user_data.username)
+    if user_data_from_db is None or user_data.password != user_data_from_db.password:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail='Invalid credentials',
+            headers={'WWW-Authenticate': 'Bearer'},
+        )
+    return {'access_token': create_token({'sub': user_data.username})}
