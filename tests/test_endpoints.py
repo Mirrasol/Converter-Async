6d032cb68041b32c728339ab8f0d1f1f2a14b5c9
Elena Dobruk <elena.dobruk@yandex.ru>
2024-12-28 12:42:34 +0300

    update user endpoints tests

--- tests/test_endpoints.py
+++ tests/test_endpoints.py
@@ -1,5 +1,6 @@
 from fastapi.testclient import TestClient
 from main import app
+from app.api.schemas.users import User
 from app.db.database import USERS_DATA
 
 client = TestClient(app)
@@ -13,8 +14,15 @@ def test_register_user_route():
         )
     assert response.status_code == 200
     assert response.json() == {'message': 'You have successfully registered!'}
-    assert new_user in USER_DATA
+    assert new_user in USERS_DATA
 
 
 def test_login_route():
-    response = client.post('/auth/login/')
+    user_data = {'username': 'Gale', 'password': 'waterdeep888'}
+    response = client.post(
+        '/auth/login/',
+        data=user_data,
+        headers={'Content-Type': 'application/x-www-form-urlencoded'},
+        )
+    assert response.status_code == 200
+    assert 'access_token' in response.json()
