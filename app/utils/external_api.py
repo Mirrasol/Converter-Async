076d02ef1ddfea987697ba91b77abcdcd83717c7
Elena Dobruk <elena.dobruk@yandex.ru>
2025-01-16 12:08:00 +0300

    add access to data from the external api

--- app/utils/external_api.py
+++ app/utils/external_api.py
@@ -1,5 +1,6 @@
 import requests
 from app.core.config import get_settings
+from app.api.schemas.currency import Currencies
 
 settings = get_settings()
 
@@ -8,9 +9,18 @@ def get_currencies_list():
     url = 'https://api.apilayer.com/currency_data/list'
     headers = {'apikey': settings.API_KEY}
     response_data = requests.get(url, headers=headers)
-    currencies_list = response_data.json()['currencies']
+    currencies_list = response_data.json().get('currencies')
     return currencies_list
 
 
-def get_current_exchange_rates():
-    pass
+def get_current_exchange_rates(currencies_data: Currencies):
+    url = 'https://api.apilayer.com/currency_data/convert'
+    params = {
+        'from': currencies_data.from_currency,
+        'to': currencies_data.to_currency,
+        'amount': currencies_data.amount,
+    }
+    headers = {'apikey': settings.API_KEY}
+    response_data = requests.get(url, params=params, headers=headers)
+    exchange_result = response_data.json().get('result')
+    return exchange_result
