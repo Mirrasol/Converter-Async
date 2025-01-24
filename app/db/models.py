05d841026187f09cc89ff84c84d2c8e2c0a81201
Elena Dobruk <elena.dobruk@yandex.ru>
2025-01-24 16:20:06 +0300

    fix linting errors

--- app/db/models.py
+++ app/db/models.py
@@ -6,8 +6,11 @@ import datetime
 
 class User(Base):
     __tablename__ = 'users'
-    
+
     id: Mapped[int] = mapped_column(BigInteger, primary_key=True, index=True)
     username: Mapped[str] = mapped_column(unique=True)
     password: Mapped[str]
-    created_at: Mapped[datetime.datetime] = mapped_column(DateTime, nullable=False, server_default=func.now())
+    created_at: Mapped[datetime.datetime] = mapped_column(
+        DateTime, nullable=False,
+        server_default=func.now(),
+    )
