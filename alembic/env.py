0ca4af983b80f79bff276b3e00fe6c92b72110b5
Elena Dobruk <elena.dobruk@yandex.ru>
2025-01-24 16:31:15 +0300

    adjust initial alembic configs for the project

--- alembic/env.py
+++ alembic/env.py
@@ -1,3 +1,6 @@
+import os
+import sys
+
 from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
@@ -5,9 +8,24 @@ from sqlalchemy import pool
 
 from alembic import context
 
+sys.path.append(os.path.join(sys.path[0], 'app'))
+
+from app.core.config import get_settings
+from app.db.database import Base
+from app.db.models import User
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+
+settings = get_settings()
+
+section = config.config_ini_section
+config.set_section_option(section, "DB_HOST", settings.DB_HOST)
+config.set_section_option(section, "DB_PORT", settings.DB_PORT)
+config.set_section_option(section, "DB_USER", settings.DB_USER)
+config.set_section_option(section, "DB_NAME", settings.DB_NAME)
+config.set_section_option(section, "DB_PASS", settings.DB_PASS)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
@@ -18,7 +36,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
