54eb2f96c10b4fa02035ee823746f8893b0098db
Elena Dobruk <elena.dobruk@yandex.ru>
2025-01-27 17:04:30 +0300

    update base user model to allow model validation for the user service

--- app/api/schemas/users.py
+++ app/api/schemas/users.py
@@ -1,8 +1,10 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
 import datetime
 
 
 class UserCreate(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
     username: str
     password: str
 
