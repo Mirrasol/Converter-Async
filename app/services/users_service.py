72cec39329dac4e8b6000bd1f6045be2b24b8355
Elena Dobruk <elena.dobruk@yandex.ru>
2025-01-29 15:58:54 +0300

    update exception handling

--- app/services/users_service.py
+++ app/services/users_service.py
@@ -2,6 +2,7 @@ from app.api.schemas.users import UserCreate, UserFromDB
 from app.utils.uow import IUnitOfWork
 from app.core.security import create_token
 from fastapi.security import OAuth2PasswordRequestForm
+from sqlalchemy.exc import NoResultFound
 from app.core.exception_handlers import InvalidCredentialsException, UserExistsException
 
 
@@ -29,8 +30,11 @@ class UserService:
 
     async def get_jwt_token(self, data: OAuth2PasswordRequestForm):
         async with self.uow:
-            user_from_db = await self.uow.user.get_one_by_name(data.username)
-            if user_from_db is None or user_from_db.password != data.password:
+            try:
+                user_from_db = await self.uow.user.get_one_by_name(data.username)
+            except (NoResultFound):
+                raise InvalidCredentialsException
+            if user_from_db.password != data.password:
                 raise InvalidCredentialsException
             current_token = create_token({"sub": data.username})
             return current_token
