3a5c3ed81ad0afb23cfb196dace8cc9eeab53ec3
Elena Dobruk <elena.dobruk@yandex.ru>
2025-01-29 12:24:06 +0300

    update custom exception handlers

--- app/utils/external_api.py
+++ app/utils/external_api.py
@@ -1,4 +1,5 @@
 import httpx
+from httpx import AsyncClient
 from app.core.config import get_settings
 from app.api.schemas.currency import Currencies
 from app.core.exception_handlers import InvalidCurrencyException, ExternalAPIException
@@ -6,26 +7,28 @@ from app.core.exception_handlers import InvalidCurrencyException, ExternalAPIExc
 settings = get_settings()
 
 
-def get_currencies_list():
-    url = 'https://api.apilayer.com/currency_data/list'
-    headers = {'apikey': settings.API_KEY}
-    response_data = httpx.get(url, headers=headers)
-    if response_data.status_code != 200:
-        raise ExternalAPIException(status_code=response_data.status_code)
-    currencies_list = response_data.json().get('currencies')
-    return currencies_list
+async def get_currencies_list():
+    with AsyncClient() as client:
+        url = 'https://api.apilayer.com/currency_data/list'
+        headers = {'apikey': settings.API_KEY}
+        response_data = await client.get(url, headers=headers)
+        if response_data.status_code != 200:
+            raise ExternalAPIException(status_code=response_data.status_code)
+        currencies_list = response_data.json().get('currencies')
+        return currencies_list
 
 
-def get_current_exchange_rates(currencies_data: Currencies):
-    url = 'https://api.apilayer.com/currency_data/convert'
-    params = {
-        'from': currencies_data.from_currency,
-        'to': currencies_data.to_currency,
-        'amount': currencies_data.amount,
-    }
-    headers = {'apikey': settings.API_KEY}
-    response_data = httpx.get(url, params=params, headers=headers)
-    if response_data.status_code != 200:
-        raise InvalidCurrencyException(status_code=response_data.status_code)
-    exchange_result = response_data.json().get('result')
-    return exchange_result
+async def get_current_exchange_rates(currencies_data: Currencies):
+    with AsyncClient() as client:
+        url = 'https://api.apilayer.com/currency_data/convert'
+        params = {
+            'from': currencies_data.from_currency,
+            'to': currencies_data.to_currency,
+            'amount': currencies_data.amount,
+        }
+        headers = {'apikey': settings.API_KEY}
+        response_data = await client.get(url, params=params, headers=headers)
+        if response_data.status_code != 200:
+            raise InvalidCurrencyException(status_code=response_data.status_code)
+        exchange_result = response_data.json().get('result')
+        return exchange_result
